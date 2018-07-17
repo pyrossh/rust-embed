d80cbff3e1c732d62d7675ea913067ba1ae63782
Hung-I Wang <whygowe@gmail.com>
2018-07-17 14:31:33 +0800

    Complete the example for actix-web

--- examples/actix.rs
+++ examples/actix.rs
@@ -1,13 +1,38 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest};
+#[macro_use]
+extern crate rust_embed;
 
-fn index(_req: HttpRequest) -> &'static str {
-    "Hello world!"
+use actix_web::{App, HttpRequest, HttpResponse, server};
+use actix_web::http::Method;
+
+#[derive(RustEmbed)]
+#[folder = "examples/public/"]
+struct Asset;
+
+fn handle_embedded_file(path: &str) -> HttpResponse {
+  match Asset::get(path) {
+    Some(content) => HttpResponse::Ok().body(content),
+    None => HttpResponse::NotFound().body("404 Not Found"),
+  }
+}
+
+fn index(_req: HttpRequest) -> HttpResponse {
+  handle_embedded_file("index.html")
+}
+
+fn dist(req: HttpRequest) -> HttpResponse {
+  let path = &req.path()["/dist/".len()..];
+  handle_embedded_file(path)
 }
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:8000")
-        .unwrap()
-        .run();
+  server::new(|| {
+    App::new().route("/", Method::GET, index).route(
+      "/dist{_:.*}",
+      Method::GET,
+      dist,
+    )
+  }).bind("127.0.0.1:8000")
+    .unwrap()
+    .run();
 }
