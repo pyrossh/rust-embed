7d7fe68a980d717ab7e6bece03c8234946fd051e
Luka Dornhecker <luka.dornhecker@gmail.com>
2018-10-15 23:11:23 +0200

    format project using cargo fmt

--- examples/rocket.rs
+++ examples/rocket.rs
@@ -5,11 +5,11 @@ extern crate rocket_contrib;
 #[macro_use]
 extern crate rust_embed;
 
-use std::path::PathBuf;
+use rocket::http::{ContentType, Status};
+use rocket::response;
 use std::ffi::OsStr;
 use std::io::Cursor;
-use rocket::response;
-use rocket::http::{ContentType, Status};
+use std::path::PathBuf;
 
 #[derive(RustEmbed)]
 #[folder = "examples/public/"]
@@ -19,32 +19,18 @@ struct Asset;
 fn index<'r>() -> response::Result<'r> {
   Asset::get("index.html").map_or_else(
     || Err(Status::NotFound),
-    |d| {
-      response::Response::build()
-        .header(ContentType::HTML)
-        .sized_body(Cursor::new(d))
-        .ok()
-    },
+    |d| response::Response::build().header(ContentType::HTML).sized_body(Cursor::new(d)).ok(),
   )
 }
 
 #[get("/dist/<file..>")]
 fn dist<'r>(file: PathBuf) -> response::Result<'r> {
   let filename = file.display().to_string();
-  let ext = file
-    .as_path()
-    .extension()
-    .and_then(OsStr::to_str)
-    .expect("Could not get file extension");
+  let ext = file.as_path().extension().and_then(OsStr::to_str).expect("Could not get file extension");
   let content_type = ContentType::from_extension(ext).expect("Could not get file content type");
   Asset::get(&filename.clone()).map_or_else(
     || Err(Status::NotFound),
-    |d| {
-      response::Response::build()
-        .header(content_type)
-        .sized_body(Cursor::new(d))
-        .ok()
-    },
+    |d| response::Response::build().header(content_type).sized_body(Cursor::new(d)).ok(),
   )
 }
 
