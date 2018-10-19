b634aaf22f41539d8fc7f33ee6d7c0898dc63288
Bernardo <berublan@gmail.com>
2018-10-19 21:47:30 +0200

    no need for separate tests since implementations are the same

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,13 +1,12 @@
 #[macro_use]
 extern crate rust_embed;
 
-#[test]
-#[cfg(debug_assertions)]
-fn dev() {
-  #[derive(RustEmbed)]
-  #[folder = "examples/public/"]
-  struct Asset;
+#[derive(RustEmbed)]
+#[folder = "examples/public/"]
+struct Asset;
 
+#[test]
+fn get_works() {
   match Asset::get("index.html") {
     None => assert!(false, "index.html should exist"),
     _ => assert!(true),
@@ -20,35 +19,10 @@ fn dev() {
     None => assert!(false, "llama.png should exist"),
     _ => assert!(true),
   }
-
-  let mut num_files = 0;
-  for file in Asset::iter() {
-    assert!(Asset::get(file.as_ref()).is_some());
-    num_files += 1;
-  }
-  assert_eq!(num_files, 6);
 }
 
 #[test]
-#[cfg(not(debug_assertions))]
-fn prod() {
-  #[derive(RustEmbed)]
-  #[folder = "examples/public/"]
-  struct Asset;
-
-  match Asset::get("index.html") {
-    None => assert!(false, "index.html should exist"),
-    _ => assert!(true),
-  }
-  match Asset::get("gg.html") {
-    Some(_) => assert!(false, "gg.html should not exist"),
-    _ => assert!(true),
-  }
-  match Asset::get("images/llama.png") {
-    None => assert!(false, "llama.png should exist"),
-    _ => assert!(true),
-  }
-
+fn iter_works() {
   let mut num_files = 0;
   for file in Asset::iter() {
     assert!(Asset::get(file.as_ref()).is_some());
