663001ddcf7d8b9c4b248d5a98b4cd4ca64ee606
Tangent 128 <Tangent128@gmail.com>
2019-01-03 22:06:58 -0500

    Define a trait describing the interface for embedded asset directories

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,38 @@ pub use rust_embed_impl::*;
 #[doc(hidden)]
 #[cfg(all(debug_assertions, not(feature = "debug-embed")))]
 pub mod utils;
+
+/// A directory of binary assets.
+///
+/// They should be embedded into the executable for release builds,
+/// but can be read from the filesystem for debug builds.
+///
+/// This trait is meant to be derived like so:
+/// ```
+/// #[macro_use]
+/// extern crate rust_embed;
+/// #[derive(RustEmbed)]
+/// #[folder = "examples/public/"]
+/// struct Asset;
+/// ```
+
+pub trait RustEmbed {
+  /// Given a relative path from the assets folder, returns the bytes if found.
+  ///
+  /// If the feature `debug-embed` is enabled or the binary is compiled in
+  /// release mode, the bytes have been embeded in the binary and a
+  /// `Cow::Borrowed(&'static [u8])` is returned.
+  ///
+  /// Otherwise, the bytes are read from the file system on each call and a
+  /// `Cow::Owned(Vec<u8>)` is returned.
+  fn get(&self, file_path: &str) -> Option<std::borrow::Cow<'static, [u8]>>;
+
+  /// Iterates the files in this assets folder.
+  ///
+  /// If the feature `debug-embed` is enabled or the binary is compiled in
+  /// release mode, a static array to the list of relative paths to the files
+  /// is used.
+  ///
+  /// Otherwise, the files are listed from the file system on each call.
+  fn iter(&self) -> Filenames;
+}
