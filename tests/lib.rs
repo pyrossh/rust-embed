a283b903d0c5e1a84c70b78f2d940bb49d79db7a
Tangent 128 <Tangent128@gmail.com>
2019-01-03 22:08:33 -0500

    Impl the RustEmbed trait on asset types

--- tests/lib.rs
+++ tests/lib.rs
@@ -31,3 +31,31 @@ fn iter_works() {
   }
   assert_eq!(num_files, 6);
 }
+
+#[test]
+fn trait_works_generic() {
+  trait_works_generic_helper(&Asset);
+}
+fn trait_works_generic_helper(folder: &impl rust_embed::RustEmbed) {
+  let mut num_files = 0;
+  for file in folder.iter() {
+    assert!(folder.get(file.as_ref()).is_some());
+    num_files += 1;
+  }
+  assert_eq!(num_files, 6);
+  assert!(folder.get("gg.html").is_none(), "gg.html should not exist");
+}
+
+#[test]
+fn trait_works_object() {
+  trait_works_object_helper(&Asset);
+}
+fn trait_works_object_helper(folder: &dyn rust_embed::RustEmbed) {
+  let mut num_files = 0;
+  for file in folder.iter() {
+    assert!(folder.get(file.as_ref()).is_some());
+    num_files += 1;
+  }
+  assert_eq!(num_files, 6);
+  assert!(folder.get("gg.html").is_none(), "gg.html should not exist");
+}
