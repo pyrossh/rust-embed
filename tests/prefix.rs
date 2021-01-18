a669d959ff8f10b4501c1ab50a3159d25cbd1851
Mcat12 <newtoncat12@yahoo.com>
2021-01-17 19:05:36 -0500

    Format prefix tests

--- tests/prefix.rs
+++ tests/prefix.rs
@@ -7,18 +7,18 @@ struct Asset;
 
 #[test]
 fn get_with_prefix() {
-    assert!(Asset::get("prefix/index.html").is_some());
+  assert!(Asset::get("prefix/index.html").is_some());
 }
 
 #[test]
 fn get_without_prefix() {
-    assert!(Asset::get("index.html").is_none());
+  assert!(Asset::get("index.html").is_none());
 }
 
 #[test]
 fn iter_values_have_prefix() {
-    for file in Asset::iter() {
-        assert!(file.starts_with("prefix/"));
-        assert!(Asset::get(file.as_ref()).is_some());
-    }
+  for file in Asset::iter() {
+    assert!(file.starts_with("prefix/"));
+    assert!(Asset::get(file.as_ref()).is_some());
+  }
 }
