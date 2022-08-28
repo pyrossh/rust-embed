b5e57f1efb75f8c7a14729c2b55375e0d6130225
frederikhors <41120635+frederikhors@users.noreply.github.com>
2022-08-28 18:45:11 +0200

    use Response everywhere

--- examples/axum-spa/main.rs
+++ examples/axum-spa/main.rs
@@ -1,9 +1,9 @@
 use axum::{
-    body::{boxed, Full},
-    handler::Handler,
-    http::{header, StatusCode, Uri},
-    response::{IntoResponse, Response},
-    routing::Router,
+  body::{boxed, Full},
+  handler::Handler,
+  http::{header, StatusCode, Uri},
+  response::Response,
+  routing::Router,
 };
 use rust_embed::RustEmbed;
 use std::net::SocketAddr;
@@ -16,57 +16,48 @@ struct Assets;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().fallback(static_handler.into_service());
+  let app = Router::new().fallback(static_handler.into_service());
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+  println!("listening on {}", addr);
+  axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
 }
 
 async fn static_handler(uri: Uri) -> Response {
-    let path = uri.path().trim_start_matches('/');
+  let path = uri.path().trim_start_matches('/');
 
-    if path.is_empty() || path == INDEX_HTML {
-        return index_html().await.into_response();
+  if path.is_empty() || path == INDEX_HTML {
+    return index_html().await;
+  }
+
+  match Assets::get(path) {
+    Some(content) => {
+      let body = boxed(Full::from(content.data));
+      let mime = mime_guess::from_path(path).first_or_octet_stream();
+
+      Response::builder().header(header::CONTENT_TYPE, mime.as_ref()).body(body).unwrap()
     }
+    None => {
+      if path.contains('.') {
+        return not_found().await;
+      }
 
-    match Assets::get(path) {
-        Some(content) => {
-            let body = boxed(Full::from(content.data));
-            let mime = mime_guess::from_path(path).first_or_octet_stream();
-
-            Response::builder()
-                .header(header::CONTENT_TYPE, mime.as_ref())
-                .body(body)
-                .unwrap()
-        }
-        None => {
-            if path.contains('.') {
-                return not_found().await.into_response();
-            }
-
-            index_html().await.into_response()
-        }
+      index_html().await
     }
+  }
 }
 
 async fn index_html() -> Response {
-    match Assets::get(INDEX_HTML) {
-        Some(content) => {
-            let body = boxed(Full::from(content.data));
+  match Assets::get(INDEX_HTML) {
+    Some(content) => {
+      let body = boxed(Full::from(content.data));
 
-            Response::builder()
-                .header(header::CONTENT_TYPE, "text/html")
-                .body(body)
-                .unwrap()
-        }
-        None => not_found().await.into_response(),
+      Response::builder().header(header::CONTENT_TYPE, "text/html").body(body).unwrap()
     }
+    None => not_found().await,
+  }
 }
 
-async fn not_found() -> impl IntoResponse {
-    (StatusCode::NOT_FOUND, "404")
+async fn not_found() -> Response {
+  Response::builder().status(StatusCode::NOT_FOUND).body(boxed(Full::from("404"))).unwrap()
 }
