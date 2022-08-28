3fc67976874ab80342c18c93fb003f55377c387b
frederikhors <41120635+frederikhors@users.noreply.github.com>
2022-08-28 17:14:09 +0200

    upd assets dir

--- examples/axum-spa/main.rs
+++ examples/axum-spa/main.rs
@@ -11,7 +11,7 @@ use std::net::SocketAddr;
 static INDEX_HTML: &str = "index.html";
 
 #[derive(RustEmbed)]
-#[folder = "assets/"]
+#[folder = "examples/axum-spa/assets/"]
 struct Assets;
 
 #[tokio::main]
