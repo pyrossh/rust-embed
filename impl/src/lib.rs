639ec4dc403aa5e0889c0eee088453bed2caf386
Buckram <buckram123@gmail.com>
2024-02-24 15:06:25 +0200

    found better function

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -143,11 +143,7 @@ fn dynamic(ident: &syn::Ident, folder_path: String, prefix: Option<&str>, includ
                   // Tried to request a path that is not in the embedded folder
 
                   // Should be allowed only if it was a symlink
-                  // TODO: Currently it allows "path_traversal_attack" for the symlink files
-                  // For it to be working properly we need to get absolute path first
-                  // and check that instead if it starts with `canonical_folder_path`
-                  // https://doc.rust-lang.org/std/path/fn.absolute.html (currently nightly)
-                  let metadata = ::std::fs::metadata(file_path.as_path()).ok()?;
+                  let metadata = ::std::fs::symlink_metadata(file_path.as_path()).ok()?;
                   if !metadata.is_symlink() {
                     return ::std::option::Option::None;
                   }
