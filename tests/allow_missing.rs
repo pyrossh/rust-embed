5655966bad6c01a4bb7ddc528fefddd66d578b46
Liran Piade <liranpiade@gmail.com>
2025-02-13 17:12:59 +1000

    Ensure examples/missing is missing

--- tests/allow_missing.rs
+++ tests/allow_missing.rs
@@ -1,3 +1,5 @@
+use std::{path::PathBuf, str::FromStr};
+
 use rust_embed::Embed;
 
 #[derive(Embed)]
@@ -7,5 +9,7 @@ struct Asset;
 
 #[test]
 fn missing_is_empty() {
+  let path = PathBuf::from_str("./examples/missing").unwrap();
+  assert!(!path.exists());
   assert_eq!(Asset::iter().count(), 0);
 }
